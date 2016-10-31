428e7afc49eb2be416502565ca1e5b9bf6e34eaa
Alex Olivas <alex.olivas11@gmail.com>
2016-10-31 16:11:48 -0500

    Removed accounts app

--- accounts/account_urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
-from views import AccountDetails
-
-urlpatterns = [
-    url(r'^$', AccountDetails.as_view(), name='account'),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- accounts/serializers/profile_serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from accounts.models import Profile
-
-
-class ProfileSerializer(serializers.HyperlinkedModelSerializer):
-
-    class Meta:
-        model = Profile
-        fields = ('bio', 'location', 'birth_date',)
