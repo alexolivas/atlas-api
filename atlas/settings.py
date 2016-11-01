dac7cba9795b61cdbbf83ddbad633b9bc488ed70
Alex Olivas <alex.olivas11@gmail.com>
2016-10-31 20:54:55 -0500

    Fixing static files to work with heroku

--- atlas/settings.py
+++ atlas/settings.py
@@ -138,3 +138,11 @@ JWT_AUTH = {
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
