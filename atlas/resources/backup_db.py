66d59ed8ed711d5fdc72eaec1766e0c769845cd9
Alex Olivas <alex.olivas11@gmail.com>
2016-11-10 14:09:04 -0500

    Database backup and refresh scripts

--- atlas/resources/backup_db.py
+++ atlas/resources/backup_db.py
@@ -1,9 +1,38 @@
+import datetime
 import os
+import socket
 
-os.system('../.././manage.py dumpdata accounts > accounts.json')
-os.system('../.././manage.py dumpdata projects > projects.json')
-os.system('../.././manage.py dumpdata web > web.json')
 
-os.system('mv accounts.json data/accounts.json')
-os.system('mv projects.json data/projects.json')
-os.system('mv web.json data/web.json')
+# Change to the project's main directory so we can use manage.py
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+os.chdir(project_dir)
+
+# Create a backup log so we have a recording of when and from where the backup took place
+logfile = open('backup.log', 'w')
+logfile.write('Last backup: ')
+logfile.write(str(datetime.datetime.now()))
+logfile.write(' from ' + str(socket.gethostname()) + '\n')
+
+# Only backup the following tables
+print('backing up web...')
+os.system('./manage.py dumpdata web > web_bkp.json')
+os.system('mv web_bkp.json atlas/resources/data/backup/web_bkp.json')
+
+print('backing up accounts...')
+os.system('./manage.py dumpdata accounts > accounts_bkp.json')
+os.system('mv accounts_bkp.json atlas/resources/data/backup/accounts_bkp.json')
+
+print('backing up projects...')
+os.system('./manage.py dumpdata projects > projects_bkp.json')
+os.system('mv projects_bkp.json atlas/resources/data/backup/projects_bkp.json')
+
+logfile.close()
+os.system('mv backup.log atlas/resources/data/backup/backup.log')
+
+# Commit the backup files into the repository
+commit_message = 'Backing up database tables from ' + str(socket.gethostname())
+os.system('git commit -m "' + commit_message + '" -- atlas/resources/data/backup')
+os.system('git push origin')
+
+# TODO: Create an endpoint to call this file and add a button to my project management UI so that backups can be
+# TODO: created with the push of a button.
--- atlas/resources/refresh_db.py
+++ atlas/resources/refresh_db.py
@@ -1,5 +1,25 @@
 import os
 
-os.system('../.././manage.py loaddata data/web.json')
-os.system('../.././manage.py loaddata data/accounts.json')
-os.system('../.././manage.py loaddata data/projects.json')
+# Change to the project's main directory
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+os.chdir(project_dir)
+
+# Only refresh this environment if the data refresh flag is turned on.
+# DO NOT RUN THIS IN PRODUCTION!!
+if os.environ.get('DATA_REFRESH', False):
+    # Drop the existing data
+    print('[Flushing Data]')
+    os.system('./manage.py flush --noinput')
+
+    # Restore this environment's database back to the development/stage state
+    print('[Running Data Refresh]')
+    print('creating users..')
+    os.system('./manage.py loaddata atlas/resources/data/refresh/users.json')
+    print('creating web content..')
+    os.system('./manage.py loaddata atlas/resources/data/refresh/web.json')
+    print('creating accounts..')
+    os.system('./manage.py loaddata atlas/resources/data/refresh/accounts.json')
+    print('creating projects..')
+    os.system('./manage.py loaddata atlas/resources/data/refresh/projects.json')
+    print('creating auth tokens')
+    os.system('./manage.py loaddata atlas/resources/data/refresh/tokens.json')
