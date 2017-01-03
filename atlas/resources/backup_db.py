a5408da95049bf1d88bf2a1bca81d4fd7a8f4cdd
Alex Olivas <alex.olivas11@gmail.com>
2017-01-03 14:48:32 -0600

    Database back up script updated

--- atlas/resources/backup_db.py
+++ atlas/resources/backup_db.py
@@ -13,26 +13,26 @@ logfile.write('Last backup: ')
 logfile.write(str(datetime.datetime.now()))
 logfile.write(' from ' + str(socket.gethostname()) + '\n')
 
-# Only backup the following tables
+# Only backup the following apps
 print('backing up web...')
-os.system('./manage.py dumpdata web > web_bkp.json')
-os.system('mv web_bkp.json atlas/resources/data/backup/web_bkp.json')
+os.system('./manage.py dumpdata web > web.json')
+os.system('mv web.json atlas/resources/data/web.json')
 
 print('backing up accounts...')
-os.system('./manage.py dumpdata accounts > accounts_bkp.json')
-os.system('mv accounts_bkp.json atlas/resources/data/backup/accounts_bkp.json')
+os.system('./manage.py dumpdata accounts > accounts.json')
+os.system('mv accounts.json atlas/resources/data/accounts.json')
 
 print('backing up projects...')
-os.system('./manage.py dumpdata projects > projects_bkp.json')
-os.system('mv projects_bkp.json atlas/resources/data/backup/projects_bkp.json')
+os.system('./manage.py dumpdata projects > projects.json')
+os.system('mv projects.json atlas/resources/data/projects.json')
 
 logfile.close()
-os.system('mv backup.log atlas/resources/data/backup/backup.log')
+os.system('mv backup.log atlas/resources/data/backup.log')
 
 # Commit the backup files into the repository
-commit_message = 'Backing up database tables from ' + str(socket.gethostname())
-os.system('git commit -m "' + commit_message + '" -- atlas/resources/data/backup')
-os.system('git push origin')
+# commit_message = 'Backing up database tables from ' + str(socket.gethostname())
+# os.system('git commit -m "' + commit_message + '" -- atlas/resources/data/backup')
+# os.system('git push origin')
 
 # TODO: Create an endpoint to call this file and add a button to my project management UI so that backups can be
 # TODO: created with the push of a button.
