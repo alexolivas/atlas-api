089e03eb581465077e6634f278b617ceef1a0e24
aolivas <alex.olivas@bluvector.io>
2018-10-18 16:32:41 -0400

    removed GitVersion dependencies

--- bitbucket-pipelines-download-heroku-cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CACHE_DIR=/usr/local/bin/heroku
-
-mkdir -p ${CACHE_DIR}
-if [ -d "${CACHE_DIR}" ]; then
-    echo "heroku cli already installed"
-else
-    curl https://cli-assets.heroku.com/install.sh | sh
-fi
